3a5c941ad549971b7f606ea7599bf4ea06640621
Manuel <carlosd99m@gmail.com>
2021-02-18 00:51:37 -0600

    Sistema rutas agregado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CrearPacienteComponent } from './pages/crear-paciente/crear-paciente.component';
+import { DatosPacienteComponent } from './pages/datos-paciente/datos-paciente.component';
+import { HistorialPacienteComponent } from './pages/historial-paciente/historial-paciente.component';
+import { NuevoHistorialComponent } from './pages/nuevo-historial/nuevo-historial.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'crear-paciente', component: CrearPacienteComponent },
+  { path: 'datos-paciente', component: DatosPacienteComponent },
+  { path: 'historial-paciente', component: HistorialPacienteComponent },
+  { path: 'nuevo-historial', component: NuevoHistorialComponent },
+
+
+  { path: '**', redirectTo: 'crear-paciente'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
